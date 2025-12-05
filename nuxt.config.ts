2be8533d2b3aa5cbe10d06827c995fa48ce1479c
wasabina67 <wasabina67@gmail.com>
2025-12-05 20:52:36 +0900

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
 
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+
   app: {
     baseURL: '/nuxt3-minimal-starter/'
   },
