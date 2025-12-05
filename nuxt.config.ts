c3e46886aa0be6361ff769a929012727dade826c
wasabina67 <wasabina67@gmail.com>
2025-12-05 20:09:57 +0900

    Update nuxt.config.ts, package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+
+  app: {
+    baseURL: '/nuxt3-minimal-starter/'
+  },
+
+  nitro: {
+    output: {
+      dir: 'docs',
+      publicDir: 'docs'
+    }
+  }
 })
